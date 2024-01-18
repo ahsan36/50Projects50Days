7ebf9c5043fc08e481b2096663ec40af1ed327a0
Ahsan <ahsaninfo1@gmail.com>
2024-01-18 12:46:58 +0530

    first commit

---
+++ Side-Navigation/script.js
@@ -0,0 +1,8 @@
+const openAndClose = document.getElementById("sideNav");
+
+function openNav() {
+    openAndClose.style.width = "250px";
+}
+function closeNav() {
+    openAndClose.style.width = "0";
+}
